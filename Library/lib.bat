b89660cef971f66b2c4dd4c70a25d2bf9bbcc1fd
Madhulina <madhuk4@APAC.DELL.COM>
2020-01-15 10:46:38 +0530

    more file added

---
+++ Library/lib.bat
@@ -0,0 +1,2 @@
+pip install pyTest
+pip install selenium
--- Test_SecondTest.py
+++ Test_SecondTest.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
 
-def test_firsttest() -> object:
+def test_secondtest() -> object:
     path = 'C:/Users/madhuk4/PycharmProjects/FirstProject/Driver/chromedriver.exe'
     driver = webdriver.Chrome(executable_path=path)
     driver.get('https://thetestingworld.com/testing')
